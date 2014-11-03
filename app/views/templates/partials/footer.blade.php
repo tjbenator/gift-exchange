20338573ee161214a5dfc1b4d63e9c92568172ec
Travis Beck <tjbenator@gmail.com>
2014-11-03 20:57:20 +0000

    Added Github buttons
    Using buttons from mdo/github-buttons

--- app/views/templates/partials/footer.blade.php
+++ app/views/templates/partials/footer.blade.php
@@ -2,6 +2,11 @@
 	<abbr title='Server Time' class='initialism'>
 		<i class='fa fa-clock-o'></i> <?php echo date('F jS, Y G:i:s e', time()); ?>
 	</abbr>
+	<br />
+	<br />
+	<iframe src="http://ghbtns.com/github-btn.html?user=tjbenator&repo=gift-exchange&type=follow&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="165" height="20"></iframe>
+	<iframe src="http://ghbtns.com/github-btn.html?user=tjbenator&repo=gift-exchange&type=fork&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="95" height="20"></iframe>
+	<iframe src="http://ghbtns.com/github-btn.html?user=tjbenator&repo=gift-exchange&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe>
 </div>
 </body>
 </html>
