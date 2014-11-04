25a91b4b55e1632d2d6e0ccbdc17aafd7602dd75
Travis Beck <tjbenator@gmail.com>
2014-11-04 05:30:58 +0000

    Set config/app.php to default values. Added Reminders Migration
    My OLD development security key is here on Github. I have a new one for production :)

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => true,
+	'debug' => false,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'ofgwA2dSwtM3sj0oi7hhvwxUiIpum1A2',
+	'key' => 'YourSecretKey!!!',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
